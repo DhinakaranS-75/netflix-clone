13a5e5bb2f24a29b4afdad4a73ee793f4b255a39
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-05-28 22:17:26 +0530

    my codes

---
+++ src/HomeScreen/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HomeScreen.css'
+
+const Homescreen = () => {
+  return (
+    <div className='homescreen'>
+      
+    </div>
+  )
+}
+
+export default Homescreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAvhXDwfyVS1exHl4dpUHl_m2VB6EzdSfk",
+    authDomain: "netflix-clone-75.firebaseapp.com",
+    projectId: "netflix-clone-75",
+    storageBucket: "netflix-clone-75.appspot.com",
+    messagingSenderId: "632575809887",
+    appId: "1:632575809887:web:78559d2a34b67cdee73eea"
+  };
